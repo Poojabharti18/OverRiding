9b6f3c9a6eb6059d9349ad01bd03feddcb2ae34e
Poojabharti18 <104221550+Poojabharti18@users.noreply.github.com>
2022-08-21 16:24:12 +0530

    Add files via upload

---
+++ Bird.java
@@ -0,0 +1,9 @@
+package overridingexample;
+
+public class Bird {
+	public void fly()
+	{
+		System.out.println("i can fly");
+	}
+
+}
---
+++ Hen.java
@@ -0,0 +1,9 @@
+package overridingexample;
+
+public class Hen {
+	public void fly()
+	{
+		System.out.println("i can't fly");
+	}
+
+}
